cb732c6b8f0f397072acfe9d1440511f2032e369
Felix Steiner <felix.steiner7@gmail.com>
2021-03-12 19:50:51 +0100

    Initial commit

---
+++ src/main/java/com/gaspricetracker/gaspricetracker/GasPriceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.gaspricetracker.gaspricetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasPriceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GasPriceTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaspricetracker/gaspricetracker/GasPriceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaspricetracker.gaspricetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasPriceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
