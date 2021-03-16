effa660f1b20fb909364cfcdcff3a963013b3a15
Felix Steiner <felix.steiner7@gmail.com>
2021-03-16 22:32:22 +0100

    Removed dummy test

--- src/test/java/com/gaspricetracker/gaspricetracker/GasPriceTrackerApplicationTests.java
+++ src/test/java/com/gaspricetracker/gaspricetracker/GasPriceTrackerApplicationTests.java
@@ -1,16 +1,7 @@
 package com.gaspricetracker.gaspricetracker;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class GasPriceTrackerApplicationTests {
-
-	@Test
-	void contextLoads() {
-		assertThat(true).isTrue();
-	}
-
 }
