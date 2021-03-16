8d7de7daddf4c227a8a177a052a73f51a589a144
Felix Steiner <felix.steiner7@gmail.com>
2021-03-16 22:23:37 +0100

    Added dummy test for testing the CI

--- src/test/java/com/gaspricetracker/gaspricetracker/GasPriceTrackerApplicationTests.java
+++ src/test/java/com/gaspricetracker/gaspricetracker/GasPriceTrackerApplicationTests.java
@@ -3,11 +3,14 @@ package com.gaspricetracker.gaspricetracker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class GasPriceTrackerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(true).isTrue();
 	}
 
 }
