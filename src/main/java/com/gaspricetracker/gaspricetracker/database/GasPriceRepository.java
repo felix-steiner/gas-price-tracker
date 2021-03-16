5bb71acbf7fa7e9b3333ad8574d149201fe08516
Felix Steiner <felix.steiner7@gmail.com>
2021-03-16 22:15:12 +0100

    Adjusted formatting

--- src/main/java/com/gaspricetracker/gaspricetracker/database/GasPriceRepository.java
+++ src/main/java/com/gaspricetracker/gaspricetracker/database/GasPriceRepository.java
@@ -2,5 +2,5 @@ package com.gaspricetracker.gaspricetracker.database;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GasPriceRepository extends  JpaRepository<GasPriceEntry, Integer> {
+public interface GasPriceRepository extends JpaRepository<GasPriceEntry, Integer> {
 }
