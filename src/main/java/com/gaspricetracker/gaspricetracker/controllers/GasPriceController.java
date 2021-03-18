63950419338d438b6116299a28676bb1900b84ce
Felix Steiner <felix.steiner7@gmail.com>
2021-03-18 20:35:27 +0100

    Added a REST endpoint for querying all gas prices in the database

---
+++ src/main/java/com/gaspricetracker/gaspricetracker/controllers/GasPriceController.java
@@ -0,0 +1,21 @@
+package com.gaspricetracker.gaspricetracker.controllers;
+
+import com.gaspricetracker.gaspricetracker.database.GasPriceEntry;
+import com.gaspricetracker.gaspricetracker.services.GasPriceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class GasPriceController {
+
+    @Autowired
+    private GasPriceService gasPriceService;
+
+    @GetMapping("/gas-prices")
+    public List<GasPriceEntry> getGasPrices() {
+        return gasPriceService.readGasPriceEntries();
+    }
+}
