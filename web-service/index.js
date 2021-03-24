db0eb631f98e17506d76aa6b87c5104250719d45
Felix Steiner <felix.steiner7@gmail.com>
2021-03-24 16:18:03 +0100

    Implemented chart visualization with Apex Charts

---
+++ web-service/index.js
@@ -0,0 +1,28 @@
+const timeseries = [];
+fetch('localhost:8080/gas-prices/chart')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(gasStation => {
+      const prices = [];
+      gasStation.gasPriceDTOS.forEach(x => {
+        prices.push({x: timestampToDate(x.createdAt), y: x.price});
+      });
+      timeseries.push({name: gasStation.name, data: prices});
+    });
+  });
+
+const options = {
+  chart: {
+    type: 'line',
+  },
+  series: timeseries,
+};
+
+const chart = new ApexCharts(document.getElementById('chart'), options);
+
+chart.render();
+
+function timestampToDate(timestamp) {
+  const date = new Date(timestamp);
+  return `${date.getDate()}.${date.getMonth()}.${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
+}
